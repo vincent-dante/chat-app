952909c91477306a0a1d39a25c2e39cc4cbb0620
Vincent Dante <vincent.dante96@gmail.com>
2021-04-23 03:38:33 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,36 @@
   <div class="side-bar">
     <h1>Chat App</h1>
 
-    <input type="text" value="" class="input-search">
+    <div class="search-container">
+      <input type="text" value="" class="input-search" placeholder="Search user...">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search search-icon" viewBox="0 0 16 16">
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+      </svg>
+    </div>
+
     
-    <div>
+    <div class="user-item-container">
       <div class="user-item">
         <img src="./assets/cat.png" alt="" srcset="" class="image">
-        <div>
+        <div class="user-name">
           <p>Cat Suu</p>
         </div>
       </div>
       <div class="user-item">
         <img src="./assets/mai-sakura.jpg" alt="" srcset="" class="image">
-        <div>
+        <div class="user-name">
           <p>Mai Sakura</p>
         </div>
       </div>
       <div class="user-item">
         <img src="./assets/spongebob.jpg" alt="" srcset="" class="image">
-        <div>
+        <div class="user-name">
           <p>SpongeBob</p>
         </div>        
       </div>
     </div>
   </div>
+
   <router-view/>
 </template>
 
@@ -58,27 +65,62 @@ body {
   padding: 3rem 10rem;
 }
 
+.search-container {
+  position: relative;
+  margin-top: 50px;
+}
+
 .input-search {
   display: block;
   width: 100%;
   border-radius: 20px;
-  border: none;
-  outline: none;
-  padding: 10px 30px;
-  background: #48485b;
-  color: #C0B9B8;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-  margin-top: 50px;
+  border: none;
+  background: #2a2a33;
+  color: #C0B9B8;
+  padding: 10px 40px;
+  outline: none;
+}
+
+.input-search:focus {
+  transition: all 0.5s;
+  background: #353544;
+}
+
+.search-icon {
+  position: absolute;
+  top: 10px;
+  left: 15px;
+  color: #C0B9B8;
 }
 
 .image {
+  width: 60px;
   border-radius: 50%;
-  width: 80px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+
+.user-item-container {
+  margin-top: 50px;
 }
 
 .user-item {
   display: flex;
   flex-wrap: wrap;
-  padding: 10px 0;
+  padding: 10px 15px;
+  align-items: center;
+}
+
+.user-item:hover {
+  transition: all 0.3s;
+  background: #1e1e26;
+  border-radius: 4px;
+  cursor: pointer;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+}
+
+.user-name {
+  font-size: 14px;
+  padding-left: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import bootstrap from 'bootstrap-icons/font/bootstrap-icons';
 
-
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(bootstrap).mount('#app')
